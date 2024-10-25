c8d2b6345ba2c94b5e526fb2434b2d76a2056180
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2024-10-24 23:42:46 -0500

    Sistema

---
+++ Sistema/src/main/java/ec/edu/espol/sistema/Sistema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.sistema;
+
+/**
+ *
+ * @author DHAMAR
+ */
+public class Sistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
